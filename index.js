c54284daffb4fdb6a1bcb1fab21212020c5e84c3
Joy <github.joysaha@gmail.com>
2024-05-22 09:28:52 +0600

    food cart get data from database

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ async function run() {
 
     //carts collection
     app.get('/carts', async(req, res) => {
-      const result = await cartsCollection.find().toArray();
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartsCollection.find(query).toArray();
       res.send(result);
     })
     
